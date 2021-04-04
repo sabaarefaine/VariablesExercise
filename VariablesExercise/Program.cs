051aae992853475c8290332c1cde54d633ca559e
Saba Arefaine <arefainesaba@gmail.com>
2021-04-04 13:17:40 -0700

    playing with variables

---
+++ VariablesExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VariablesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare and initialize variables
+            string catName = "Nala";
+            char nickName = 'T';
+            bool lovesTreats = true;
+            double catToys = 16;
+            decimal weight = 12.8m;
+
+            //string interpolation
+            Console.WriteLine($"Let me introduce you to my cat, {catName}, but sometimes we call her {nickName} which is short for Teddy, her middle name. She enjoys playing with her many cat toys, {catToys} to be exact and especially loves her treats, it's {lovesTreats}. That's probably why her chunky self is {weight}lbs, but we" +
+                $"re working on it!");
+        }
+    }
+}
